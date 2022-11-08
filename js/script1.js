edd7f7f1cdda0fc207558a8521d59e1d55796532
cleber ribeiro <cleberhill@gmail.com>
2022-11-08 09:14:04 -0300

    create add product with valid.., list/del products

--- js/script1.js
+++ js/script1.js
@@ -1,15 +1,86 @@
 class Produto {
   constructor() {
-    this.id = 0;
-    this.nomeProduto = "";
-    this.preco = 0;
+    this.id = 1;
+    this.arrayProdutos = [];
   }
 
   salvar() {
-    alert("Adicionando um produto!");
+    let produto = this.lerDados();
+
+    if (this.validaCampos(produto)) {
+      // alert("salvar");
+      this.adicionar(produto);
+    }
+
+    console.log(this.arrayProdutos);
+    this.listaTabela();
+    this.cancelar();
   }
-  excluir() {
-    alert("Excluindo um produto!");
+
+  listaTabela() {
+    let tbody = document.getElementById("tbody");
+    tbody.innerText = "";
+
+    for (let i = 0; i < this.arrayProdutos.length; i++) {
+      let tr = tbody.insertRow();
+
+      let td_id = tr.insertCell();
+      let td_produto = tr.insertCell();
+      let td_preco = tr.insertCell();
+      let td_acoes = tr.insertCell();
+
+      td_id.innerText = this.arrayProdutos[i].id;
+      td_produto.innerText = this.arrayProdutos[i].nomeProduto;
+      td_preco.innerText = this.arrayProdutos[i].preco;
+
+      td_id.classList.add("center");
+
+      let imgEdit = document.createElement("img");
+      imgEdit.src = "img/edit.png";
+
+      let imgDel = document.createElement("img");
+      imgDel.src = "img/delete.png";
+
+      td_acoes.appendChild(imgEdit);
+      td_acoes.appendChild(imgDel);
+    }
+  }
+
+  adicionar(produto) {
+    this.arrayProdutos.push(produto);
+    this.id++;
+  }
+
+  lerDados() {
+    let produto = {};
+
+    produto.id = this.id;
+    produto.nomeProduto = document.getElementById("produto").value;
+    produto.preco = document.getElementById("preco").value;
+
+    return produto;
+  }
+
+  validaCampos(produto) {
+    let msg = "";
+    if (produto.nomeProduto == "") {
+      msg += " - Informe o Nome do Produto \n";
+    }
+    if (produto.preco == "") {
+      msg += " - Informe o Preço do Produto \n";
+    }
+
+    if (msg != "") {
+      alert(msg);
+      return false;
+    }
+    return true;
+  }
+
+  cancelar() {
+    document.getElementById("produto").value = "";
+    document.getElementById("preco").value = "";
   }
 }
-var produto = new Produto();
+
+const produto = new Produto();
